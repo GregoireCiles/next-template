85942401b09e4ffc2a310bd7647808dd08ff7dd0
Gregoire Ciles <gregoire.c@obsidius.agency>
2023-05-18 16:30:19 +0200

    chore: update site config

--- config/site.ts
+++ config/site.ts
@@ -3,8 +3,8 @@ import type { SiteConfig } from '@/types/config';
 export const siteConfig: SiteConfig = {
   name: 'Next Template',
   description: 'A Next.js 13 template for building apps with Tailwind CSS.',
-  url: 'http://localhost:3000',
-  ogImage: 'http://localhost:3000/og.jpg',
+  url: 'https://next-template-woad.vercel.app',
+  ogImage: 'https://next-template-woad.vercel.app/og.jpg',
   links: {
     github: 'https://github.com/GregoireCiles/next-template',
     twitter: 'https://twitter.com/GregoireCiles',
