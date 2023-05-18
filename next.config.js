25129c8faec66629aa36f8701f8bda89324f17c5
Gregoire Ciles <gregoire.c@obsidius.agency>
2023-05-18 16:23:32 +0200

    chore: update next.js config

--- next.config.js
+++ next.config.js
@@ -5,12 +5,11 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
-    optimizeCss: true,
     legacyBrowsers: false,
   },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
